701004bd56dd70363ec4cbdf137853608f244185
kachanovski <58528758+kachanovski@users.noreply.github.com>
2020-10-23 22:01:34 +0300

    Updates

---
+++ precache-manifest.9dd09bf258553c845c21b648475d99f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915d043d396b5c3370cc7121b50a145",
+    "url": "/weather-project/index.html"
+  },
+  {
+    "revision": "a06624f6744052674eef",
+    "url": "/weather-project/static/css/main.b1961dd9.chunk.css"
+  },
+  {
+    "revision": "ed1002901fe4a338c11e",
+    "url": "/weather-project/static/js/2.6c69ae9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-project/static/js/2.6c69ae9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06624f6744052674eef",
+    "url": "/weather-project/static/js/main.88f13921.chunk.js"
+  },
+  {
+    "revision": "7660c3011e17619fbd2f",
+    "url": "/weather-project/static/js/runtime-main.2f4005a3.js"
+  },
+  {
+    "revision": "4dbcba736947748c17a4f4c2f211842d",
+    "url": "/weather-project/static/media/photo-1411538.4dbcba73.jpg"
+  },
+  {
+    "revision": "ec3c3d60be146ca5a2445a1ccccccba0",
+    "url": "/weather-project/static/media/search.ec3c3d60.png"
+  },
+  {
+    "revision": "1c5976ad4b7135ae1a74dc1874167789",
+    "url": "/weather-project/static/media/zakat-vecher-solnce-oblaka-nebo-luchi.1c5976ad.svg"
+  }
+]);
